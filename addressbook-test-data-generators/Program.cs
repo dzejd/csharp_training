d2a8d93bb4eaa645270cabbbc1f69fd9cb656730
topoltseva <dtopoltseva@vizex.ru>
2019-05-20 11:45:15 +0300

    Занятие 6

---
+++ addressbook-test-data-generators/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAdressbookTests;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.CSharp;
+
+namespace addressbook_test_data_generators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(args[0]);
+            string filename = args[1];
+            StreamWriter writer = new StreamWriter(args[1]);
+            string format = args[3];
+
+            List<GroupData> groups = new List<GroupData>();
+            for (int i = 0; i < count; i++)
+            {
+                groups.Add(new GroupData(TestBase.GenerateRandomString(10))
+                {
+                    Header = TestBase.GenerateRandomString(100),
+                    Footer = TestBase.GenerateRandomString(100)
+                });
+            }
+            if (format == "excel")
+            {
+                WriteGroupsToExcelFile(groups, filename);
+            }
+            else
+            {
+                StreamWriter writer1 = new StreamWriter(filename);
+                if (format == "csv")
+                {
+                    WriteGroupsToCSVFile(groups, writer1);
+                }
+                else if (format == "xml")
+                {
+                    WriteGroupsToXMLFile(groups, writer1);
+                }
+                else if (format == "json")
+                {
+                    WriteGroupsToJsonFile(groups, writer1);
+                }
+                else
+                {
+                    Console.Out.Write("Bad format" + format);
+                }
+                writer1.Close();
+            }
+        }
+
+        static void WriteGroupsToExcelFile(List<GroupData> groups, string filename)
+        {
+            Excel.Application app = new Excel.Application();
+            app.Visible = true;
+            Excel.Workbook wb = app.Workbooks.Add();
+            Excel.Worksheet sheet = wb.ActiveSheet;
+
+            int row = 1;
+            foreach(GroupData group in groups)
+            {
+                sheet.Cells[row, 1] = group.Name;
+                sheet.Cells[row, 2] = group.Header;
+                sheet.Cells[row, 3] = group.Footer;
+                row++;
+            }
+            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), filename);
+            File.Delete(fullPath);
+            wb.SaveAs(Path.Combine(Directory.GetCurrentDirectory(), filename));
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
+        }
+
+        static void WriteGroupsToCSVFile(List<GroupData> groups, StreamWriter writer)
+        {
+            foreach (GroupData group in groups)
+            {
+                writer.WriteLine(string.Format("${0},${1},${2}",
+                    group.Name, group.Header, group.Footer));
+            }
+        }
+
+        static void WriteGroupsToXMLFile(List<GroupData> groups, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
+        }
+
+        static void WriteGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented));
+        }
+    }
+}
--- addressbook-web-tests/Model/GroupData.cs
+++ addressbook-web-tests/Model/GroupData.cs
@@ -13,6 +13,10 @@ namespace WebAdressbookTests
             Name = name;
         }
 
+        public GroupData()
+        {
+        }
+
         public bool Equals(GroupData other)
         {
             if (Object.ReferenceEquals(other, null))
